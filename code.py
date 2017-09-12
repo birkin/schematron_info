678e367e6c0068d86c245700b2f4e587a49d9329
birkin <birkin.diana@gmail.com>
2017-09-12 14:45:56 -0400

    modernizes code.

--- code.py
+++ code.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""
+Demo of how to use schematron with lxml.
+    - example from <http://lxml.de/validation.html>
+"""
+
+import sys
 import lxml
 from lxml import etree, isoschematron
 
+
+assert sys.version_info >= ( 3,0 )  # just to show this works with v3x
+
+
 ## make docs
-schematron_doc = etree.parse( open(u'./schematron.xml') )  # type(schematron_doc) == lxml.etree._ElementTree
-bad_doc = etree.parse( open(u'./source_bad.xml') )
-good_doc = etree.parse( open(u'./source_good.xml') )
+with open( './schematron.xml', 'r' ) as fh:
+    schematron_doc = etree.parse( fh )
+with open( './source_bad.xml', 'r' ) as fh:
+    bad_doc = etree.parse( fh )
+with open( './source_good.xml', 'r' ) as fh:
+    good_doc = etree.parse( fh )
 
 ## make the schematron test-object
 schematron = isoschematron.Schematron( schematron_doc )  # type(schematron) == lxml.isoschematron.Schematron
@@ -17,15 +30,26 @@ assert result == False
 result = schematron.validate( good_doc )
 assert result == True
 
-## examine output
+
+"""
+that's it -- but you can examine the output, too
+fyi:
+    - type(schematron_2.validation_report) == lxml.etree._XSLTResultTree
+    - type(report) == bytes
+"""
+
 schematron_2 = isoschematron.Schematron( schematron_doc, store_report=True )
-#
-result_2 = schematron_2.validate( bad_doc )  # type(schematron_2.validation_report) == lxml.etree._XSLTResultTree
-assert result_2 == False
-report = etree.tostring( schematron_2.validation_report, pretty_print=True )  # type(report) == str
-print( 'REPORT:\n%s\n\n' % report.decode(u'utf8', u'replace') )
-#
-result_2 = schematron_2.validate( good_doc )
-assert result_2 == True
+
+result = schematron_2.validate( bad_doc )
+assert result == False
 report = etree.tostring( schematron_2.validation_report, pretty_print=True )
-print( 'REPORT:\n%s\n\n' % report.decode(u'utf8', u'replace') )
+print( '--------------------' )
+print( 'bad doc report, ```\n%s```' % report.decode('utf-8') )
+
+result = schematron_2.validate( good_doc )
+assert result == True
+report = etree.tostring( schematron_2.validation_report, pretty_print=True )
+print( '--------------------' )
+print( 'good doc report, ```\n%s```' % report.decode('utf-8') )
+
+print( '--------------------' )
